1a625b589b94cf5b93fad21d7e0a96dede6cc042
RohithBoppey <rohithboppey1298@gmail.com>
2022-10-26 23:06:10 +0530

    merged

--- client/src/components/Admin/AdminLogin.js
+++ client/src/components/Admin/AdminLogin.js
@@ -4,14 +4,13 @@ import "./adLogin.css";
 import { useNavigate } from "react-router-dom";
 
 const AdminLogin = (props) => {
-
 	const submitHandler = (event) => {
 		event.preventDefault();
 		const details = {
 			username: usernameRef.current.value,
-			password: passwordRef.current.value
-		}
-		props.onSignin(details)
+			password: passwordRef.current.value,
+		};
+		props.onSignin(details);
 	};
 
 	const usernameRef = useRef();
@@ -19,19 +18,35 @@ const AdminLogin = (props) => {
 
 	return (
 		<form className="login-ad" action="./" method="POST">
-           
-            <div className="content-ad">
-                <div className="header-ad"><h2>Admin Login</h2></div>
-                <label><h5>Email</h5></label>
+			<div className="content-ad">
+				<div className="header-ad">
+					<h2>Admin Login</h2>
+				</div>
+				<label>
+					<h5>Email</h5>
+				</label>
 				<br></br>
-                <input className="input-ad" type="email" name="_email" placeholder="Email"></input>
-				<br></br><br></br>
-                <label><h5>Password</h5></label>
+				<input
+					className="input-ad"
+					type="email"
+					name="_email"
+					placeholder="Email"></input>
 				<br></br>
-                <input className="input-ad" type="password" name="_password" placeholder="Password"></input>
-                <button className="button-ad" type="submit">Login</button>
-            </div>
-        </form>
+				<br></br>
+				<label>
+					<h5>Password</h5>
+				</label>
+				<br></br>
+				<input
+					className="input-ad"
+					type="password"
+					name="_password"
+					placeholder="Password"></input>
+				<button className="button-ad" type="submit">
+					Login
+				</button>
+			</div>
+		</form>
 		// </div>
 	);
 };
